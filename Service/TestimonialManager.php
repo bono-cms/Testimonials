2937f5a1d1a9a55e79088dccfc75457edbbddb5a
David Yang <daworld.ny@gmail.com>
2015-12-18 22:52:43 +0500

    Added service skeleton

---
+++ Service/TestimonialManager.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Testimonials\Service;
+
+use Cms\Service\AbstractManager;
+
+final class TestimonialManager extends AbstractManager implements TestimonialManagerInterface
+{
+}
