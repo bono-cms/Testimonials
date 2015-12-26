b569a56bfec36c50892aa16d10aa806cf7f6286a
David Yang <daworld.ny@gmail.com>
2015-12-26 21:41:23 +0500

    Added missing AUTO_INCREMENT

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,7 +1,7 @@
 
 DROP TABLE IF EXISTS `bono_module_testimonials`;
 CREATE TABLE `bono_module_testimonials` (
-    `id` INT NOT NULL PRIMARY KEY,
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `lang_id` INT NOT NULL,
     `author` varchar(100),
     `content` TEXT   
