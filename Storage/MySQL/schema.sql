0dbbf214473b6fdd73340eb81fde3c5198e1439f
David Yang <daworld.ny@gmail.com>
2015-12-27 21:17:50 +0500

    Added missing columns in the schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -3,6 +3,8 @@ DROP TABLE IF EXISTS `bono_module_testimonials`;
 CREATE TABLE `bono_module_testimonials` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `lang_id` INT NOT NULL,
+    `published` varchar(1) NOT NULL,
+    `order` INT NOT NULL,
     `author` varchar(100),
     `content` TEXT   
 ) DEFAULT CHARSET = UTF8;
