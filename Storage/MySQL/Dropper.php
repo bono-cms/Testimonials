6ae9a0f8f26747dbc17f210a6e3603bd1a489ce3
David Yang <daworld.ny@gmail.com>
2016-07-13 23:09:19 +0500

    Added storage dropper

---
+++ Storage/MySQL/Dropper.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Testimonials\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractStorageDropper;
+
+final class Dropper extends AbstractStorageDropper
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTables()
+    {
+        return array(
+            TestimonialMapper::getTableName()
+        );
+    }
+}
