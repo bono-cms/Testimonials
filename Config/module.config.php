09dc4202a51251e57ff1cefa1188fc59b6322f75
David Yang <daworld.ny@gmail.com>
2016-08-01 22:30:12 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,19 +1,8 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Testimonials',
-        'route' => 'Testimonials:Admin:Testimonial@gridAction',
-        'icon'  => 'fa fa-mixcloud fa-5x',
-        'description' => 'This module lets you manage testimonials on your site'
-    )
+ 'name'  => 'Testimonials',
+ 'route' => 'Testimonials:Admin:Testimonial@gridAction',
+ 'icon'  => 'fa fa-mixcloud fa-5x',
+ 'description' => 'This module lets you manage testimonials on your site'
 );
