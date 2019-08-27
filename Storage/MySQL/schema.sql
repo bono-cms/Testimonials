c0465544f119a0721b732781a1595158c545b2e3
dave-yang <daworld.ny@gmail.com>
2019-08-28 02:02:02 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -4,12 +4,15 @@ CREATE TABLE `bono_module_testimonials` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `published` varchar(1) NOT NULL,
     `order` INT NOT NULL
-) DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_testimonials_translations`;
 CREATE TABLE `bono_module_testimonials_translations` (
     `id` INT NOT NULL,
     `lang_id` INT NOT NULL,
     `author` varchar(100),
-    `content` LONGTEXT   
-) DEFAULT CHARSET = UTF8;
+    `content` LONGTEXT,
+
+    FOREIGN KEY (id) REFERENCES bono_module_testimonials(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
